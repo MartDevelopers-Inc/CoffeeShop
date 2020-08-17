4c381162fcfa4d1c15f1ac9afb89ea31d636e64d
MartMbithi <martdevelopers254@gmail.com>
2020-08-17 16:17:54 +0300

    Added Configs

---
+++ resources/views/Coffee_Pos_Core/admin/partials/_scripts.php
@@ -0,0 +1,5 @@
+  <!-- Core -->
+  <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Argon JS -->
+  <script src="assets/js/argon.js?v=1.0.0"></script>
