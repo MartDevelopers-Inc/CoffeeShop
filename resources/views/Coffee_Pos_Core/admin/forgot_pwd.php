49cfc3145fbac330dd1087a0c3f398034b1280fa
MartMbithi <martdevelopers254@gmail.com>
2020-08-17 18:21:57 +0300

    Implemented Password Resets

--- resources/views/Coffee_Pos_Core/admin/forgot_pwd.php
+++ resources/views/Coffee_Pos_Core/admin/forgot_pwd.php
@@ -1,4 +1,43 @@
 <?php
+session_start();
+include('config/config.php');
+require_once('config/code-generator.php');
+
+if(isset($_POST['reset_pwd']))
+{
+    if(!filter_var($_POST['reset_email'], FILTER_VALIDATE_EMAIL))
+    {
+        $err = 'Invalid Email';
+    }
+    $checkEmail = mysqli_query($mysqli, "SELECT `admin_email` FROM `coffee_shop_admin` WHERE `admin_email` = '".$_POST['reset_email']."'") or exit(mysqli_error($mysqli));
+    if( mysqli_num_rows($checkEmail) > 0 )  {
+        //exit('This email is already being used');
+        //Reset Password
+        $reset_code = $_POST['reset_code'];
+        $reset_token = sha1(md5($_POST['reset_token']));
+        $reset_status = $_POST['reset_status'];
+        $reset_email = $_POST['reset_email'];
+        $query="INSERT INTO coffee_shop_pass_resets (reset_email, reset_code, reset_token, reset_status) VALUES (?,?,?,?)";
+        $reset = $mysqli->prepare($query);
+        $rc=$reset->bind_param('ssss', $reset_email, $reset_code, $reset_token, $reset_status);
+        $reset->execute();
+        if($reset)
+        {
+            $success = "Password Reset Instructions Sent To Your Email";
+            // && header("refresh:1; url=index.php");
+        }
+        else
+        {
+            $err = "Please Try Again Or Try Later";
+        }
+         
+    }
+    else 
+    {
+        $err = "No account with that email";
+    }
+        
+}
     require_once('partials/_head.php');
 ?>
 <body class="bg-default">
@@ -30,11 +69,15 @@
                     <input class="form-control" required name="reset_email" placeholder="Email" type="email">
                   </div>
                 </div>
+                <div style="display:none">
+                    <input type="text" value="<?php echo $tk;?>" name="reset_token">
+                    <input type="text" value="<?php echo $rc;?>" name="reset_code">
+                    <input type="text" value="Pending" name="reset_status">
+                </div>
                 <div class="text-center">
-                  <button type="button" name="reset_pwd" class="btn btn-primary my-4">Reset Password</button>
+                  <button type="submit" name="reset_pwd" class="btn btn-primary my-4">Reset Password</button>
                 </div>
               </form>
-
             </div>
           </div>
           <div class="row mt-3">
