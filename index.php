8d2c63220d492038c9ec599575abf2d9817b0718
MartMbithi <martdevelopers254@gmail.com>
2020-08-19 14:40:49 +0300

    Whipped Admin Module and All Underlying Functionalities

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/Coffee/resources/views/Coffee_Pos_Core/admin/');
+	header('Location: '.$uri.'/CoffeeShop/resources/views/Coffee_Pos_Core/admin/');
 	exit;
 ?>
 Something is wrong here and i can feel it
