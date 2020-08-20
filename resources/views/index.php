b699d4361da85b89d5ceee624cbc79dec87d2cac
MartMbithi <martdevelopers254@gmail.com>
2020-08-20 16:09:45 +0300

    Refacotored Views For Deployment

--- resources/views/index.php
+++ resources/views/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>iCoffee</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -84,18 +84,13 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Laravel
+                iCoffee Pos
             </div>
 
             <div class="links">
-                <a href="https://laravel.com/docs">Docs</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://blog.laravel.com">Blog</a>
-                <a href="https://nova.laravel.com">Nova</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://vapor.laravel.com">Vapor</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
+                <a href="iCoffee/admin/">Admin Sign In</a>
+                <a href="iCoffee/cashier/">Cashier Sign In</a>
+                <a href="iCoffee/customer">Customer Sign In</a>
             </div>
         </div>
     </div>
